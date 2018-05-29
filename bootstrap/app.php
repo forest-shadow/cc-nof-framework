360da43f794e2c3a46e1337c7565b396cfe99116
forest-shadow <mamasay@yandex.ru>
2018-05-29 10:04:33 +0300

    Add phpdotenv module

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,3 +5,12 @@ session_start();
 
 // requiring auto-loading feature
 require_once __DIR__ . '/../vendor/autoload.php';
+
+try {
+    $dotenv = (new Dotenv\Dotenv(__DIR__ . '/..//'))->load();
+} catch (Dotenv\Exception\InvalidPathException $e) {
+    //
+}
+
+
+var_dump(getenv('APP_NAME'));
